14a6eeb0310f789abe88130d998bccffe986d71b
Nmags7 <31768170+Nmags7@users.noreply.github.com>
2019-05-28 21:44:03 -0500

    Add files via upload

---
+++ Lightning/sketch_180919a/LightningJS/LightningJS.js
@@ -0,0 +1,33 @@
+var startX=0;
+var startY=150;
+var endX=0;
+var endY=150;
+function setup()
+{
+  createCanvas(300, 300);
+  strokeWeight(2);
+  background(155);
+
+  strokeWeight(10);
+}
+function draw()
+{
+  background(155);
+  //stroke((int)(Math.random()));
+  while (endX<300) {
+    endX=startX+(int)(Math.random()*10);
+    endY=startY+(int)(Math.random()*19)-9;
+    line(startX, startY, endX, endY);
+    startX=endX;
+    startY=endY;
+    console.log("startX " +startX);
+    console.log("startY " +startY);
+  }
+}
+function mousePressed()
+{
+  startX=0;
+  startY=150;
+  endX=0;
+  endY=150;
+}
